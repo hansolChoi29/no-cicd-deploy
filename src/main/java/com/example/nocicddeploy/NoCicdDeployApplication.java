7570a39ef4a3e3491e88e86ab0e015308f19a272
hansolChoi29 <ggksthf29@gmail.com>
2026-02-04 20:11:08 +0900

    first commit

---
+++ src/main/java/com/example/nocicddeploy/NoCicdDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.nocicddeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoCicdDeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoCicdDeployApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nocicddeploy/NoCicdDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nocicddeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoCicdDeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
